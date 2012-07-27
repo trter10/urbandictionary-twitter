ef92ee83d1a1421a43ba9616ebadb559760c2063
Aaron Peckham <apeckham@gmail.com>
2012-07-27 16:34:23 -0700

    refactor

--- parser.rb
+++ parser.rb
@@ -1,8 +1,13 @@
 class Parser
+  REGEXPS = [
+    /what(?: the fuck| the hell)? does "?([\w\s]+)"? (mean|actually mean)/i,
+    /what(?: the fuck| the hell)? is an? "?([\s\w]+)"?\?/i
+  ]
+
   def self.parse(string)
-    out = string[/what(?: the fuck| the hell)? does "?([\w\s]+)"? (mean|actually mean)/i, 1]
-    out ||= string[/what(?: the fuck| the hell)? is an? "?([\s\w]+)"?\?/i, 1]
-    return nil if %w[it that].include? out
-    out
+    REGEXPS.find { |regexp| string =~ regexp }
+    match = $1
+    return nil if %w[it that].include? match
+    match
   end
 end
