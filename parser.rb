85b5a85a90c0660df9fd2ce8e98a99fcb808f0fc
Aaron Peckham <apeckham@gmail.com>
2012-07-27 16:31:04 -0700

    regex improvements

--- parser.rb
+++ parser.rb
@@ -1,7 +1,7 @@
 class Parser
   def self.parse(string)
-    out = string[/what(?: the fuck| the hell)? does "?(\w+)"? mean\?/i, 1]
-    out ||= string[/what(?: the fuck| the hell)? is an? "?(\w+)"?\?/i, 1]
+    out = string[/what(?: the fuck| the hell)? does "?([\w\s]+)"? (mean|actually mean)/i, 1]
+    out ||= string[/what(?: the fuck| the hell)? is an? "?([\s\w]+)"?\?/i, 1]
     return nil if %w[it that].include? out
     out
   end
