7bd0264f44e3d09fabe9261db6662ec963c870fe
Aaron Peckham <apeckham@gmail.com>
2012-07-29 10:13:46 -0700

    parser

--- parser_spec.rb
+++ parser_spec.rb
@@ -26,6 +26,7 @@ describe Parser do
     @urbandictionary X Y
     @urbandictionary 'X Y'
     @urbandictionary "X Y"
+    What The Fuck Does X Y mean?
   END
 
   context "matches" do
@@ -46,6 +47,7 @@ describe Parser do
     what does that really mean?
     what is IT?
     @nobody whatever
+    Omg my mom says "what does 69 mean " and my brothers explaining it to her but it's totally wrong lmaoo..
   END
 
   context "doesn't match" do
@@ -56,40 +58,4 @@ describe Parser do
       end
     end
   end
-  
-  # it { Parser.parse("this means nothing").should be_nil }
-  # 
-  # describe "what does" do
-  #   it { Parser.parse("what does bootylicious mean?").should == "bootylicious" }
-  #   it { Parser.parse("what does \"bootylicious\" mean?").should == "bootylicious" }
-  #   it { Parser.parse("what does it mean?").should be_nil }
-  #   it { Parser.parse("what does that mean?").should be_nil }
-  #   it { Parser.parse("what does THAT mean?").should be_nil }
-  #   it { Parser.parse("what does that even mean?").should be_nil }
-  #   it { Parser.parse("what does this mean?").should be_nil }
-  # end
-  # 
-  # describe "what is" do
-  #   it { Parser.parse("what is an elephant?").should == "elephant" }
-  #   it { Parser.parse("what is a elephant?").should == "elephant" }
-  #   it { Parser.parse("what is a \"big elephant\"?").should == "big elephant" }
-  #   it { Parser.parse("@urbandictionary big elephant").should == "big elephant" }
-  #   xit { Parser.parse("Laughing like shit, corny af RT @OhhSoo_COLD: What Does Lls Mean ?").should be_nil }
-  # end
-  # 
-  # describe "define" do
-  #   it { Parser.parse("Define \"pig sty\".").should == "pig sty" }
-  #   it { Parser.parse("accept no one's definition of your life. define yourself.").should be_nil }
-  # end
-  # 
-  # describe "what the fuck/hell is" do
-  #   it { Parser.parse("what the fuck is an elephant?").should == "elephant" }
-  #   it { Parser.parse("@urbandictionary what the fuck is an elephant?").should == "elephant" }
-  #   it { Parser.parse("What The Fuck Does MLRT mean?").should == "MLRT" }
-  #   it { Parser.parse("what the hell is a elephant?").should == "elephant" }
-  #   it { Parser.parse("@jayl3n FUCK what does two strands mean in the first place").should be_nil }
-  #   it { Parser.parse("FUCK what does two strands mean in the first place").should == "two strands" }
-  #   it { Parser.parse("What does \"Champion of the Earth\" actually mean/what the fuck do they do?!").should == "Champion of the Earth" }
-  #   it { Parser.parse("Omg my mom says \"what does 69 mean \" and my brothers explaining it to her but it's totally wrong lmaoo..").should be_nil }
-  # end
 end
