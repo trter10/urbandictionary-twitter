ca52e693609bca5b13c41a541d0d4595fe3d5a33
Aaron Peckham <apeckham@gmail.com>
2012-07-29 09:55:03 -0700

    simplify parser

--- parser.rb
+++ parser.rb
@@ -1,16 +1,15 @@
 class Parser
   REGEXPS = [
-    /^[^"]*what(?: the fuck| the hell)? does "?([\w\s]+)"? (mean|actually mean)/i,
-    /^[^"]*what(?: the fuck| the hell)? is an? "?([\s\w]+)"?\?/i,
-    /^define "?([\w\s]+)"?\.?$/i,
-    /@urbandictionary (.+)/i
+    # /^[^"]*what(?: the fuck| the hell)? does "?([\w\s]+)"? (mean|actually mean)/i,
+    # /^[^"]*what(?: the fuck| the hell)? is an? "?([\s\w]+)"?\?/i,
+    # /^define "?([\w\s]+)"?\.?$/i,
+    # /@urbandictionary (.+)/i
+    /^what is ([\w\s]+)\?$/,
+    /^what does ([\w\s]+) mean\?$/
   ]
 
   def self.parse(string)
-    return nil if string =~ /^@/ && string !~ /^@urbandictionary/
     REGEXPS.find { |regexp| string =~ regexp }
-    match = $1
-    return nil if match =~ /^it|that|this|that even|she|he$/i
-    match
+    $1
   end
 end
