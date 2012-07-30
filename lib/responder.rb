f724409a24414c5f8b1f6585b225130127f434f9
Aaron Peckham <apeckham@gmail.com>
2012-07-30 10:42:34 -0700

    add logging

--- lib/responder.rb
+++ lib/responder.rb
@@ -15,7 +15,7 @@ class Responder
       if result["result_type"] == "exact" && !result["list"].empty?
         response = Formatter.format(status.from_user, result["list"].first)
         Twitter.update(response, in_reply_to_status_id: status.id) if ENV["UPDATE_TWITTER"]
-        puts "UPDATE #{response.inspect}"
+        puts "UPDATE #{response.inspect} (in_reply_to #{status.id.inspect})"
       end
     end
   end
