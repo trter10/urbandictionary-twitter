26c5dbc00e8a8159bd9b388b1aea970a3d7c123b
Aaron Peckham <apeckham@gmail.com>
2012-07-30 09:26:53 -0700

    ignore thanks

--- parser.rb
+++ parser.rb
@@ -18,7 +18,7 @@ class Parser
     if (match = $1)
       match.strip!
       match.sub!(/ anyway$/, '')
-      match unless match =~ /^(it|it all|it even|that|this|that even|that really|air|today|going on|up|happening|good)$/i
+      match unless match =~ /^(it|it all|it even|that|this|that even|that really|air|today|going on|up|happening|good|thx|thanks|thank you|thankyou)$/i
     end
   end
 end
